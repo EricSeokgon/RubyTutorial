2cf790c85e2f694424daca8516d36778ba58a71a
hadesleee <leesk55@gmail.com>
2015-06-05 16:07:45 +0900

    특수문자 제어 테스트

--- ex3/capitalize.rb
+++ ex3/capitalize.rb
@@ -3,4 +3,11 @@ class Capitalize
   puts('hello world'.upcase())
   puts('hello world'.length())
   puts('Hello world'.sub('world','programming'))
+  puts("hedeslee's \"tutorial\"")
+  puts("\\")
+  puts("Hello \nworld")
+  puts("Hello \tworld")
+  puts("Hello \t\tworld")
+  puts("\a")
+  puts('Hello\nworld')
 end
