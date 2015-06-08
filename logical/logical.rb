b92d6df064e49d49493b96631a02e643edce1ee6
hadesleee <leesk55@gmail.com>
2015-06-08 16:17:46 +0900

    logical add

---
+++ logical/logical.rb
@@ -0,0 +1,11 @@
+class Logical
+  puts("입력해주세요.")
+  in_str = gets.chomp()
+  real_hadeslee = "hadeslee"
+  real_k8805 = "k8805"
+  if real_hadeslee == in_str or real_k8805 == in_str
+      puts("Hello!!! "+in_str)
+  else
+      puts("Who are you?")
+  end
+end
