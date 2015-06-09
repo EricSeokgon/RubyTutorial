f73d467448ae8b05c0c10296d8198d4698f1e0e9
hadesleee <leesk55@gmail.com>
2015-06-09 10:21:36 +0900

    주석 study

---
+++ comment/comment.rb
@@ -0,0 +1,18 @@
+class Comment
+end
+=begin
+조건문 예제
+hadeslee
+2015
+=end
+
+input = 33
+real_hadeslee = 11
+#real_k8805 = "ab"
+if real_hadeslee == input
+  puts("Hello!!!, hadeslee")
+#elsif real_k8805 == input
+#  puts("Hello!!!, k8805")
+else
+  puts("Who are you?")
+end
