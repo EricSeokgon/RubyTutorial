293011aa4abd5245096d618c27a855ba91f49cab
hadesleee <leesk55@gmail.com>
2015-07-20 13:16:52 +0900

    객체지향 예제

---
+++ OOP/1.rb
@@ -0,0 +1,13 @@
+name1 = String.new('hadeslee')
+name2 = String.new('k8805')
+
+puts(name1.reverse())
+puts(name2.reverse())
+puts(name1.upcase())
+puts(name1.size())
+
+names = Array.new()
+names.push('hadeslee')
+names.push('k8805')
+puts(names)
+puts(names.join(','))
