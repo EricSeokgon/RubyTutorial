be7745685b94595610e5827e3ec637b4e72673cd
hadesleee <leesk55@gmail.com>
2015-07-21 10:39:53 +0900

    get/set 메소드 생성

--- OOP_Variable/1.rb
+++ OOP_Variable/1.rb
@@ -5,10 +5,18 @@ class C
   def show()
     p @value
   end
+  def getValue()
+    return @value
+  end
+  def setValue(v)
+    @value = v
+  end
 end
 
 c1 = C.new(10)
 #p c1.value
+p c1.getValue()
 #c1.value = 20
-
-c1.show()
+c1.setValue(20)
+#c1.show()
+p c1.getValue()
